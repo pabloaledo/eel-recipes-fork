3d8e037ecd1a753a4eafca9556606a16f449fbef
Pablo <pablo.aledo@seqera.io>
2022-12-01 11:35:36 +0100

    setup conda

--- cicd/build_recipes.sh
+++ cicd/build_recipes.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-echo -e "\e[34m Available recipes \e[0m"
-ls recipes
+setup_conda(){
+    echo -e "\e[34m Setup conda \e[0m"
+    (
+    curl -O https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-$(arch).sh
+    bash ./Anaconda3-2022.10-Linux-$(arch).sh -b -p $HOME/anaconda3
 
-echo '====='
-git status
-git log
-echo '====='
+    echo 'export PATH=~/anaconda3/bin:$PATH' >> ~/.bashrc
+    source ~/.bashrc
 
+    conda config --system --add channels defaults
+    conda config --system --add channels conda-forge
+    )
+}
+
+build_recipe(){
+    echo -e "\e[34m Building recipe $line \e[0m"
+    (cd "recipes/$1"; conda build .)
+}
+
+
+[ -e ~/anaconda3 ] || setup_conda
 git diff --name-only HEAD^ HEAD | grep '/recipes/' | cut -d/ -f2 | sort | uniq | while read line
 do
-    echo -e "\e[34m Building recipe $line \e[0m"
-    (cd "recipes/$line"; conda build .)
+    build_recipe "$line"
 done
