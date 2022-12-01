3eb5b758b08691b48564c00e26c3cf2f63ae547a
Pablo <pablo.aledo@seqera.io>
2022-12-01 09:57:44 +0100

    fix

--- cicd/build_recipes.sh
+++ cicd/build_recipes.sh
@@ -3,7 +3,7 @@
 echo -e "\e[34m Available recipes \e[0m"
 ls recipes
 
-git diff --name-only HEAD^ HEAD | grep recipes | cut -d/ -f2 | sort | uniq | while read line
+git diff --name-only HEAD^ HEAD | grep '/recipes/' | cut -d/ -f2 | sort | uniq | while read line
 do
     echo -e "\e[34m Building recipe $line \e[0m"
     (cd "recipes/$line"; conda build .)
